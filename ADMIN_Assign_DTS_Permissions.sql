bc834bb29189d0d7aa677f9f63565db763b7bb0a
JohnKNess <john.ness@gmx.com>
2019-08-09 15:46:16 +0200

    add roles and tables permissions for SSIS

--- ADMIN_Assign_DTS_Permissions.sql
+++ ADMIN_Assign_DTS_Permissions.sql
@@ -10,7 +10,7 @@
   Name........:	ADMIN_Assign_DTS_Permissions.sql 
   Description.:	After uncommenting one of the commented out EXEC lines
   ............	this script assigns the permissions to an account that
-  ............	are required to run DTS packages correctly.
+  ............	are required to run DTS or SSIS packages correctly.
   ............		
   ............	Please run on the target computer.
   ............		
@@ -25,12 +25,24 @@ CREATE USER [DOMAIN\ACCOUNT] FOR LOGIN [DOMAIN\ACCOUNT]
 GO
 USE [msdb]
 GO
-/*******************************************
- * uncomment one of the permissions (admin not on Production!!) 
- *******************************************/
+
+
+/* ===================================================================
+    DTS Package permissions
+ ================================================================== */
+
+/* ******************************************
+  uncomment one of the permissions (admin not on Production!!) 
+ ****************************************** */
+
+----------------------
+-- SQL Server >= 2005
+----------------------
 --exec sp_addrolemember @rolename='db_dtsadmin', @membername='DOMAIN\ACCOUNT'    -- Admin        = Enumerate All / View All / Execute All / Export All / Execute in SQL Agent / Import / Delete All / Change All
 --exec sp_addrolemember @rolename='db_dtsltduser', @membername='DOMAIN\ACCOUNT'  -- LtdUser      = Enumerate All / View Own / Execute Own / Export Own /                      / Import / Delete Own / Change Own
 --exec sp_addrolemember @rolename='db_dtsoperator', @membername='DOMAIN\ACCOUNT' -- Operator     = Enumerate All / View All / Execute All /            /                      /        /            /           
+
+
 GO
 USE [msdb]
 GO
@@ -59,4 +71,46 @@ GO
 USE [msdb]
 GO
 GRANT VIEW DEFINITION ON [dbo].[sysdtspackages] TO [DOMAIN\ACCOUNT]
+GO
+
+/* ===================================================================
+    SSIS Package permissions
+ ================================================================== */
+
+----------------------
+-- SQL Server >= 2008
+----------------------
+-- exec sp_addrolemember @rolename='db_ssisadmin', @membername='DOMAIN\ACCOUNT'    -- Admin        = Enumerate All / View All / Execute All / Export All / Execute in SQL Agent / Import / Delete All / Change All
+-- exec sp_addrolemember @rolename='db_ssisltduser', @membername='DOMAIN\ACCOUNT'  -- LtdUser      = Enumerate All / View Own / Execute Own / Export Own /                      / Import / Delete Own / Change Own
+-- exec sp_addrolemember @rolename='db_ssisssisoperator', @membername='DOMAIN\ACCOUNT' -- Operator     = Enumerate All / View All / Execute All /            /                      /        /            /           
+
+
+GO
+USE [msdb]
+GO
+GRANT ALTER ON [dbo].[sysssispackages] TO [DOMAIN\ACCOUNT]
+GO
+USE [msdb]
+GO
+GRANT DELETE ON [dbo].[sysssispackages] TO [DOMAIN\ACCOUNT]
+GO
+USE [msdb]
+GO
+GRANT INSERT ON [dbo].[sysssispackages] TO [DOMAIN\ACCOUNT]
+GO
+USE [msdb]
+GO
+GRANT SELECT ON [dbo].[sysssispackages] TO [DOMAIN\ACCOUNT]
+GO
+USE [msdb]
+GO
+GRANT TAKE OWNERSHIP ON [dbo].[sysssispackages] TO [DOMAIN\ACCOUNT]
+GO
+USE [msdb]
+GO
+GRANT UPDATE ON [dbo].[sysssispackages] TO [DOMAIN\ACCOUNT]
+GO
+USE [msdb]
+GO
+GRANT VIEW DEFINITION ON [dbo].[sysssispackages] TO [DOMAIN\ACCOUNT]
 GO
