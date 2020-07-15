52506627607c603bd36fdcd8aae20fcc6be25f60
John K Ness <JohnKNess@users.noreply.github.com>
2020-07-15 09:51:59 +0200

    update spdeletehistory.sql with license

--- spdeletehistory.sql
+++ spdeletehistory.sql
@@ -1,8 +1,26 @@
-
     use msdb
     go
+    
 
     CREATE PROCEDURE [dbo].[spdeletehistory]
+    /*
+        spdeletehistory - Wrapper for sp_delete_backuphistory stored procedure
+        Copyright (C) 2020  hot2use / JohnKNess
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.    
+    */
+    
     /*
     -- ==================================================================
     -- Author......:    hot2use / JohnKNess
@@ -25,6 +43,8 @@
     --                    Tabstop Values = 4                    
     -- ==================================================================
     */
+    
+    
     
         /*input variables*/
         @iDaysBackToStart int = 0,
